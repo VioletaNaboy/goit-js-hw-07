80f20b0112d2715398c0fc489ec9042aa520de58
VioletaNaboy <vnaboishchykova@gmail.com>
2023-05-04 16:15:01 +0200

    gallery-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const elGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", elGallery);
+gallery.addEventListener("click", selectImg);
+function selectImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName === "IMG") {
+    const selectedImg = event.target.dataset.source;
+    const instance = basicLightbox.create(
+      `
+    <img src="${selectedImg}" alt="${event.target.alt}"> `
+    );
+    instance.show();
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "Escape") {
+        instance.close();
+        document.removeEventListener();
+      }
+    });
+  } else {
+    return;
+  }
+}
